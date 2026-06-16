a25074c6cb41e5e8e6b5bd5a08b86337e517cc96
Илья <ilahorosij5822@gmail.com>
2026-06-16 14:38:21 +0300

    Отправка Exel чека на почту

--- shop/urls.py
+++ shop/urls.py
@@ -14,9 +14,10 @@ urlpatterns = [
     path('cart/add/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
     path('cart/update/<int:item_id>/', views.update_cart, name='update_cart'),
     path('cart/remove/<int:item_id>/', views.remove_from_cart, name='remove_from_cart'),
-    
+    path('checkout/', views.checkout, name='checkout'),
     # Авторизация
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('checkout/success/', views.checkout_success, name='checkout_success'),
 ]
 # ← Убери отсюда весь код со static(...) если он там есть!
